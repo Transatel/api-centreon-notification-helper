bea256e07f4f975a9d129a3556d5e82a1169f015
jordan.besly <jordan.besly@transatel.com>
2019-11-04 14:32:09 +0100

    Hostname regexp as param, handle case '.' in it

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+$hotnameRegex = '[A-Za-z0-9_/.-]+';
+
 
 // ------------------------------------------------------------------------
 // NOTIFICATION
@@ -22,15 +24,15 @@ Route::get('/', function () {
 // used for conditionally including graph in notifications
 Route::get('notification-helper/has-metrics/{host_service_id}','NotificationHelper\MetricsController@hasMetrics');
 
-Route::get('notification-helper/host-associated-poller/{host_name}','NotificationHelper\HostController@getPollerIdFromHostName')->where('host_name', '[A-Za-z0-9_/-]+');
+Route::get('notification-helper/host-associated-poller/{host_name}','NotificationHelper\HostController@getPollerIdFromHostName')->where('host_name', $hotnameRegex);
 
-Route::get('notification-helper/host-status/{host_name}','NotificationHelper\HostController@getStatus')->where('host_name', '[A-Za-z0-9_/-]+');
-Route::get('notification-helper/service-status/{host_name}/{service_description}','NotificationHelper\ServiceController@getStatus')->where('host_name', '[A-Za-z0-9_/-]+');
+Route::get('notification-helper/host-status/{host_name}','NotificationHelper\HostController@getStatus')->where('host_name', $hotnameRegex);
+Route::get('notification-helper/service-status/{host_name}/{service_description}','NotificationHelper\ServiceController@getStatus')->where('host_name', $hotnameRegex);
 
-Route::get('notification-helper/is-acked-host/{host_name}','NotificationHelper\HostController@isAcked')->where('host_name', '[A-Za-z0-9_/-]+');
-Route::get('notification-helper/ack-host/{host_name}','NotificationHelper\HostController@ackHost')->where('host_name', '[A-Za-z0-9_/-]+');
-Route::get('notification-helper/unack-host/{host_name}','NotificationHelper\HostController@unackHost')->where('host_name', '[A-Za-z0-9_/-]+');
+Route::get('notification-helper/is-acked-host/{host_name}','NotificationHelper\HostController@isAcked')->where('host_name', $hotnameRegex);
+Route::get('notification-helper/ack-host/{host_name}','NotificationHelper\HostController@ackHost')->where('host_name', $hotnameRegex);
+Route::get('notification-helper/unack-host/{host_name}','NotificationHelper\HostController@unackHost')->where('host_name', $hotnameRegex);
 
-Route::get('notification-helper/is-acked-service/{host_name}/{service_description}','NotificationHelper\ServiceController@isAcked')->where('host_name', '[A-Za-z0-9_/-]+');
-Route::get('notification-helper/ack-service/{host_name}/{service_description}','NotificationHelper\ServiceController@ackService')->where('host_name', '[A-Za-z0-9_/-]+');
-Route::get('notification-helper/unack-service/{host_name}/{service_description}','NotificationHelper\ServiceController@unackService')->where('host_name', '[A-Za-z0-9_/-]+');
+Route::get('notification-helper/is-acked-service/{host_name}/{service_description}','NotificationHelper\ServiceController@isAcked')->where('host_name', $hotnameRegex);
+Route::get('notification-helper/ack-service/{host_name}/{service_description}','NotificationHelper\ServiceController@ackService')->where('host_name', $hotnameRegex);
+Route::get('notification-helper/unack-service/{host_name}/{service_description}','NotificationHelper\ServiceController@unackService')->where('host_name', $hotnameRegex);
